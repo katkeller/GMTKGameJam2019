2981c05a37e9433bed2e3f3ebdc85ac971a66567
Kat Keller <kat.kek.keller@gmail.com>
2019-08-02 16:36:51 -0500

    Created UI keyboard with buttons that add to input string, and a send button that invokes an "InputEntered" event

---
+++ DrunkTextSim/Assets/Scripts/Keyboard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Keyboard : MonoBehaviour
+{
+    [SerializeField]
+    private Text inputField, playerTextMessage;
+
+    [SerializeField]
+    private Image playerTextMessageImage;
+
+    [SerializeField]
+    private AudioClip typeClip, sendClip;
+
+    private string input;
+    private AudioSource audioSource;
+
+    public static event Action<string> InputEntered;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        playerTextMessageImage.enabled = false;
+        playerTextMessage.text = "";
+        inputField.text = "";
+    }
+
+    public void AddLetterToInput( string letter)
+    {
+        input = input + letter;
+        inputField.text = input;
+        audioSource.PlayOneShot(typeClip);
+    }
+
+    public void SendMessage()
+    {
+        audioSource.PlayOneShot(sendClip);
+        InputEntered?.Invoke(input);
+        playerTextMessage.text = input;
+        playerTextMessageImage.enabled = true;
+        input = "";
+    }
+}
